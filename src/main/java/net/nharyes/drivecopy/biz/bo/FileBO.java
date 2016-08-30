d2ac1230d611265b5a1602fbefd2e4af057daef8
Gherynos <gherynos@nharyes.net>
2016-08-30 10:31:52 +0100

    Updated copyright dates

--- src/main/java/net/nharyes/drivecopy/biz/bo/FileBO.java
+++ src/main/java/net/nharyes/drivecopy/biz/bo/FileBO.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 Luca Zanconato
+ * Copyright 2012-2016 Luca Zanconato
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
